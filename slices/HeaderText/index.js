2d007caee69b98d289fbe9ea2279bc79475962ae
Cinzya <cynti-ebert@web.de>
2021-02-01 19:28:05 +0100

    added htmlSerializer to slices

--- slices/HeaderText/index.js
+++ slices/HeaderText/index.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { array, shape } from 'prop-types';
-import { RichText, Image } from 'prismic-reactjs';
+import { Link, RichText } from 'prismic-reactjs';
+import linkResolver from '../../prismic';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
+import htmlSerializer from '../../htmlSerializer';
 
-const h2 = {
-  color: '#8592e0',
-};
+import '../../styles/globals.css';
 
 const MySlice = ({ slice }) => (
-  <section style={section}>
-    {/* {
+  <section className="text-center">
+    {
       slice.primary.portrait ?
-      <Image render={slice.primary.portrait}/>
+      <img className="rounded-full mx-auto" src={slice.primary.portrait.url} alt={slice.primary.portrait.alt} />
       : <p>Here should be an image</p>
-    } */}
+    }
     {
       slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
+      <RichText htmlSerializer={htmlSerializer} render={slice.primary.title}/>
+      : <h2 className="text-center">Template slice, update me!</h2>
     }
     {
       slice.primary.slogan ?
-      <RichText render={slice.primary.slogan}/>
-      : <h1 style={h1}>Template slice, update me!</h1>
+      <RichText htmlSerializer={htmlSerializer} render={slice.primary.slogan}/>
+      : <h1>Template slice, update me!</h1>
       }
     {
       slice.primary.description ?
-      <RichText render={slice.primary.description}/>
+      <RichText htmlSerializer={htmlSerializer} render={slice.primary.description}/>
       : <p>start by editing this slice from inside the SliceMachine builder!</p>
+    }
+    {
+      slice.primary.contact ?
+      <a className="my-7 rounded-md bg-purple-800 text-white md:py-4 md:px-10" href={Link.url(slice.primary.contact, linkResolver)}>Go to page</a>
+      : <button>I'm a button</button>
     }
   </section>
 );
